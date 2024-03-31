2c5ac0c67d709da35aee16ce791a4d29c0f84619
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-03-31 23:32:30 +0500

    Completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -17,14 +17,14 @@ const Home = () => {
                     />
                 </div>
 
-                <div className='flex flex-col justify-center items-center relative ml-16'>
+                <div className='flex flex-col justify-center items-center relative md:ml-16'>
 
-                    <img src={mobile} alt="mobile" className="w-68 h-52 pl-1 mt-[-10px] md:w-78 md:h-50 md:mt-[-75px]" />
+                    <img src={mobile} alt="mobile" className="w-68 h-52 pl-1 mt-12 ml-11 md:w-78 md:h-50 md:mt-[-75px]" />
 
-                    <h1 className='font-Poppins font-semibold text-4xl pt-10 px-12 md:pt-6'>Simple pricing for your business</h1>
+                    <h1 className='font-Poppins font-semibold md:text-4xl text-3xl pt-16 pl-12 md:px-12 md:pt-6'>Simple pricing for your business</h1>
                     <p className='font-Poppins font-normal pt-8 md:pt-5 px-12 pr-20'>Plans that are carefully crafted to suit your business.</p>
                     {/* <button className='bg-buttonc text-white md:mt-8 md:mr-20 md:px-12 md:py-2 px-20 py-3 mt-12 mr-8'>Get Started</button> */}
-                    <button className='bg-buttonc text-white md:mt-8 md:mr-20 md:px-12 md:py-2 px-20 py-3 mt-12 mr-8'>
+                    <button className='bg-buttonc text-white md:mt-8 md:mr-20 md:px-12 md:py-2 px-24 py-4 mt-12 mr-5 ml-5'>
                         <Link to="/PricingPage">Get Started</Link>
                     </button>
                 </div>
--- src/Components/PricingPage.jsx
+++ src/Components/PricingPage.jsx
@@ -9,7 +9,7 @@ const PricingPage = () => {
         <div class="flex flex-col min-h-screen bg-white">
             <div class="flex-grow bg-buttonc h-16">
                 <div className='flex flex-col justify-center items-center'>
-                    <img src={man} alt="man" className='w-50 h-32 mt-5' />
+                    <img src={man} alt="man" className='w-70 h-48 mt-24 md:w-50 md:h-32 md:mt-5' />
                     <h1 className='font-Poppins font-extrabold text-white text-3xl pt-5'>Premium PRO</h1>
                     <h1 className='font-Poppins font-extrabold text-white text-3xl leading-10'>$329</h1>
                     <p className='font-Poppins font-medium text-white text-lg'>billed just once</p>
@@ -19,12 +19,12 @@ const PricingPage = () => {
 
 
 
-            <div class="flex-grow flex justify-center mb-16 pt-2 relative">
-                <div className='absolute container bg-gray-200 text-black'>
-                    <h1 className='font-Poppins font-medium text-sm pt-7 pl-2'>Access these features when you get this pricing package for your business.</h1>
+            <div class="flex-grow flex justify-center pt-10 md:mb-16 md:pt-2 relative">
+                <div className='absolute md:w-64 md:h-72 w-[370px] rounded-xl bg-gray-200 text-black'>
+                    <h1 className='font-Poppins font-medium md:text-sm text-lg pt-7 pl-2'>Access these features when you get this pricing package for your business.</h1>
 
-                    <div className='text-xs mt-5 pl-5 text-pcolor'>
-                        <div>
+                    <div className='md:text-xs mt-5 pl-5 text-pcolor'>
+                        <div className=''>
                             <p className='flex items-center py-1'>
                                 <FontAwesomeIcon icon={faCheck} className='mr-2 text-buttonc fa-fw' /> International calling and messaging API
                             </p>
@@ -42,20 +42,20 @@ const PricingPage = () => {
                     </div>
 
                     <div className='pt-8 flex flex-row'>
-                        <div className='flex items-center justify-between'>
+                        <div className='flex items-center justify-between pb-2'>
                             {/* icon */}
                             <div>
-                                <FontAwesomeIcon icon={faShoppingCart} className='bg-buttonc py-2 px-3 ml-4 mt-2' />
+                                <FontAwesomeIcon icon={faShoppingCart} className='bg-buttonc py-2 px-3 ml-8 h-5 md:ml-4 mt-2' />
                             </div>
                             {/* button */}
                             <div>
-                                <button className='bg-buttonc text-white md:px-12 py-2 ml-4 font-Poppins font-medium'>Buy Now</button>
+                                <button className='bg-buttonc text-white px-20 ml-5 md:px-12 py-2 md:ml-4 font-Poppins font-medium'>Buy Now</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='flex flex-row space-x-5 justify-evenly pt-4 pb-2'>
+            <div className='flex flex-col md:flex-row md:space-x-5 space-y-3 justify-evenly pt-4 pb-2 md:pl-0 pl-5'>
                 <div className='flex flex-row space-x-2'>
                     <FontAwesomeIcon icon={faCommentsDollar} />
                     <p className='font-Poppins font-medium'>30 days money back Guarantree</p>
